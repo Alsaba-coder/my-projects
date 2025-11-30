fe02d6f7ad23977d0d6dccac0d9b1c9c338ba4c9
Alsaba Khan <alsabakhan@Alsabas-MacBook-Pro-2.local>
2025-11-30 14:32:44 -0500

    changes to fix for landing photo issue.

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/alsabakhan/Desktop/coding_workspace/GS_blog_webapp/app/layout.tsx
+// File: /Users/alsabakhan/dev/github/singh-project/gs-blog-project/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
